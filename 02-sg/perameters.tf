be8901a84f44b8d4b065fea22e515f167336c2e4
rajesh <rajeshveeranki478@gmail.com>
2024-04-01 13:36:33 +0530

    daws-76s

--- 02-sg/perameters.tf
+++ 02-sg/perameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "vpn_sg_id" {
   name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
   type  = "String"
-  value = module.vpn.sg_id
+  value = module.vpc.sg_id
 }
